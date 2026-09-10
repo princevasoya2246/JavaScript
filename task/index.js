2b60a3b9c04669ef680d3bd5d29f304575f06314
Vasoyaprince2246 <princevasoya503@gmail.com>
2026-09-10 12:45:22 +0530

    basic task

---
+++ task/index.js
@@ -0,0 +1,22 @@
+console.log("hello c")
+document.write("hello m")
+
+console.log(document.getElementById("h1"))
+console.log(document.getElementById("h2"));
+console.log(document.getElementById("h3"));
+console.log(document.getElementById("h4"));
+console.log(document.getElementById("h5"));
+console.log(document.getElementById("h6"));
+console.log(document.getElementById("h1").innerHTML);
+console.log(document.getElementById("h2").innerHTML);
+console.log(document.getElementById("h3").innerHTML);
+console.log(document.getElementById("h4").innerHTML);
+console.log(document.getElementById("h5").innerHTML);
+console.log(document.getElementById("h6").innerHTML);
+
+document.getElementById("h1").innerHTML="hello h1";
+document.getElementById("h2").innerHTML="hello h2";
+document.getElementById("h3").innerHTML="hello h3";
+document.getElementById("h4").innerHTML="hello h4";
+document.getElementById("h5").innerHTML="hello h5";
+document.getElementById("h6").innerHTML="hello h6";
