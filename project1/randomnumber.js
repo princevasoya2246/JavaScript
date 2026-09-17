b4e397f4adc2dfb9ca434c51286e35b7c26b73a7
Vasoyaprince2246 <princevasoya503@gmail.com>
2026-09-17 15:48:24 +0530

    random number

--- project1/randomnumber.js
+++ project1/randomnumber.js
@@ -1,23 +1,23 @@
-var istrue=true;
-while(istrue){
-var number=Math.floor(Math.random()*100);
+var istrue = true;
+while (istrue) {
+    var number = Math.floor(Math.random() * 100);
 
-var count=10;
-while(true){
-    count--;
-    if(count==0){
-        alert("You have run out of guesses!");
-        break;
+    var count = 10;
+    while (true) {
+        count--;
+        if (count == 0) {
+            alert("You have run out of guesses!");
+            break;
+        }
+        var n = Number(prompt("enter a number 1 and 100: "));
+        if (n > number) {
+            alert(`guess the number is smaller (remaining guesses: ${count})`);
+        } else if (n == number) {
+            alert("correct!");
+            break;
+        } else {
+            alert(`guess the number is larger (remaining guesses: ${count})`);
+        }
     }
-    var n=Number(prompt("enter a number 1 and 100: "));
-    if(n>number){
-        alert(`guess the number is smaller (remaining guesses: ${count})`);
-    } else if(n==number){
-          alert("correct!");
-        break;
-    } else {
-        alert(`guess the number is larger (remaining guesses: ${count})`);
-    }
-}
-var istrue=confirm("Do you want to play again?");
+    var istrue = confirm("Do you want to play again?");
 }
